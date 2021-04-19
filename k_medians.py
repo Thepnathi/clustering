0199480b6a96ac396812f97992951598ff4a5b54
Thepnathi <thepnathi@hotmail.com>
2021-04-19 20:04:32 +0100

    Finish k-medians algorithm

--- k_medians.py
+++ k_medians.py
@@ -13,7 +13,7 @@ class K_Medians(Cluster):
         
         convergence_steps = 0
         feature_belongs_to_cluster = []
-        while convergence_steps < 2:    # Repeat step 2 and 3 until convergence (no object has moved between clusters or by specified iteration)
+        while True:    # Repeat step 2 and 3 until convergence (no object has moved between clusters or by specified iteration)
             convergence_steps += 1
             feature_belongs_to_cluster_temp = [] 
             # Step 2 - Assignment phase. Assign each feature or data object to closest cluster representative
@@ -26,7 +26,7 @@ class K_Medians(Cluster):
                         min_dist, closest_cluster_rep = distance, ith_cluster
                 feature_belongs_to_cluster_temp.append(closest_cluster_rep) # We append the index of the cluster for this feature index
             # Step 3 - Optimisation phase. Calculate new cluster representative - median point in each clusters
-            cluster_representative = self.compute_new_cluster_representatives(cluster_representative, feature_belongs_to_cluster)
+            cluster_representative = self.compute_new_cluster_representatives(cluster_representative, feature_belongs_to_cluster_temp)
             if feature_belongs_to_cluster_temp == feature_belongs_to_cluster:
                 break # Break the While loop once the object no moved between clusters
             feature_belongs_to_cluster = feature_belongs_to_cluster_temp
